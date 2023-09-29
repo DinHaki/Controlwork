0fcbaf6cac3d175f8867e112d73eefc45744fa7b
DinHaki <132045326+DinHaki@users.noreply.github.com>
2023-09-29 18:45:33 +0300

    Add files via upload

---
+++ Lastyrok/Last.cs
@@ -0,0 +1,24 @@
+string[] array1 = new string[6] {"257", "45", "hello", "world", "res","456"};
+string[] array2 = new string[array1.Length];
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}   
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArray(array1, array2);
+PrintArray(array2);
